996ab5f12a2e613695f25b8c8e30abaa2976ea1f
itsmeprinceyt <114930668+itsmeprinceyt@users.noreply.github.com>
2024-04-29 23:36:04 +0530

    added Data Handler Page

---
+++ public/script.js
@@ -0,0 +1,20 @@
+const allButtons = document.querySelectorAll(".header-tag-card-button");
+
+allButtons.forEach((button,index)=>{
+    button.addEventListener('click',()=>{
+        takeMeToNextPage(index);
+    })
+})
+
+function takeMeToNextPage(index)
+{
+    if (index === 0){
+        window.open('/',"_self");
+    }else if(index === 1){
+        window.open('https://discord.gg/spHgh4PGzF');
+    }else if(index === 2){
+        window.open('/Data-Handler/new',"_self");
+    }else if(index === 3){
+        window.open('/Data-handler/details',"_self");
+    }
+}
